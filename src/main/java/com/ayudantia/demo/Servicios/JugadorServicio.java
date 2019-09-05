2508d8406df927495fc172f8afea760080830f90
Sebastian Godinez <sebastian.godinez@sansano.usm.cl>
2019-09-05 01:02:06 -0400

    agregue el nomre del servicio

--- src/main/java/com/ayudantia/demo/Servicios/JugadorServicio.java
+++ src/main/java/com/ayudantia/demo/Servicios/JugadorServicio.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service("serviciojugador")
 public class JugadorServicio{
 
     @Autowired
