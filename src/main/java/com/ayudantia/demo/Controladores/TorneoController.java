212695fa3e36c43b49215f0a8f2c9b1c49fbb38f
godinezseba <sebastian.godinez@sansano.usm.cl>
2019-10-22 02:28:16 -0300

    CROS validation

--- src/main/java/com/ayudantia/demo/Controladores/TorneoController.java
+++ src/main/java/com/ayudantia/demo/Controladores/TorneoController.java
@@ -10,6 +10,7 @@ import com.ayudantia.demo.Servicios.TorneoServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,11 +18,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/torneo") // "localhost:8000/v1/torneo/*"
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class TorneoController{
     @Autowired
     @Qualifier("serviciotorneo")
