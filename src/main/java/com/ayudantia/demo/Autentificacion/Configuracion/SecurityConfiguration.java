4ea2e4928c186f7f1d2e626bdc96bca669241d6b
godinezseba <sebastian.godinez@sansano.usm.cl>
2019-12-18 02:49:38 -0300

    recibe un JWT

--- src/main/java/com/ayudantia/demo/Autentificacion/Configuracion/SecurityConfiguration.java
+++ src/main/java/com/ayudantia/demo/Autentificacion/Configuracion/SecurityConfiguration.java
@@ -2,15 +2,16 @@ package com.ayudantia.demo.Autentificacion.Configuracion;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     
     @Autowired
@@ -23,13 +24,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // http.csrf().disable();
+        http.csrf().disable();
         http.authorizeRequests()
             .antMatchers("/v1/torneo").hasRole("ADMIN")
             .antMatchers("/v1/equipo").hasRole("USER")
-            .antMatchers("/").permitAll()
-            .anyRequest().authenticated()
-            .and().httpBasic();
+            .antMatchers("/autentificar").permitAll()
+            .anyRequest().authenticated();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
     }
 
     @Bean
