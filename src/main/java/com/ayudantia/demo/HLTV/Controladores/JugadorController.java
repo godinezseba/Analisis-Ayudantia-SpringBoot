ee13fe4ed6a62e0058f1037ca4ca5845fb601a64
Sebastián Godínez <sgodinez@nursoft.cl>
2020-05-19 13:57:36 -0400

    eliminate login

--- src/main/java/com/ayudantia/demo/HLTV/Controladores/JugadorController.java
+++ src/main/java/com/ayudantia/demo/HLTV/Controladores/JugadorController.java
@@ -9,6 +9,7 @@ import com.ayudantia.demo.HLTV.Servicios.JugadorServicio;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,11 +17,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/jugador")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class JugadorController{
     @Autowired
     @Qualifier("serviciojugador")
